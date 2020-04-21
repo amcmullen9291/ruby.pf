dcaf67429c6b32e692ffdea364143fa6978f2c19
amcmullen9291 <61699306+amcmullen9291@users.noreply.github.com>
2020-04-20 20:07:36 -0400

    final scraper updates

--- clinteraction.rb
+++ clinteraction.rb
@@ -1,170 +1,65 @@
-#!/usr/bin/rubys
+require 'pry'
 require 'colorize'
-require 'catpix_mini'
 
-require_relative "scraper.rb"
-# require_relative "commentary.rb"
+require_relative 'scraper'
+require_relative 'commentary'
 
-# class Interaction
-#     attr_accessor :welcome, :stadiums, :response
+ class Clinteraction
 
-#     def initialize
-#         @respone = response
-#     end
+    def initialize
+    end
 
-    def welcome
-        @welcome = welcome
         puts ""
         puts ""
         puts "#{'Welcome to FootBall Central!'.blue}"
         puts ""
         puts "For a list of all Stadiums type 'list'".red
-        puts "For stadiums ranked by their size, type 'size'"
+        puts "For a list of stadiums ranked by their size, type 'size'"
+        #### maybe do options: top 5, btm 5, all
         puts "To see stadiums ranked by their opening year, type 'year'"
+        #### options: oldest, newest, all
         puts "To select a stadium by team name, type 'teams'"
         puts ""
         puts "To search by the stadium name type 'stadium'"
-        puts "type 'stop' to exit"
-        response = gets.downcase.chomp
+        puts "   OR"
+        puts "Enter your zip code to see Stadiums around you!"
+        puts ""
+        puts ""
+        puts "type #{'stop'.light_red} to exit"
+        response = gets.chomp
 
-        if response == "list"
-            @stadiums
-        elsif response == "size"
-            @size_ranking
-        elsif response == "year"
-            @year_ranking
-        elsif response == "teams"
-            @teams
-        elsif response == "stadium"
-            @stadium_name
-        elsif response == "stop"
-            exit[0]
+    (response =) case
+        when "list"
+            Scraper.all.surface.map.with_index do |name, index|
+                puts "#{index}: #{name}"
+            end
+        when teams
+            Scraper.teams.each_with_index do |name, index|
+                puts "#{index}: #{name}"
+            end
         else
-            puts "Please try again."
-            response = gets.chomp
-            timeout = 1
-            if response != "size" && response != "year" && response != "team" &&response != "stadium" && response != "list"
-                3.times do
-                puts " Invalid response. Please try again."
-                response = gets.chomp
-                timeout +=1
-                    if timeout == 4
-                        puts "goodbye"
-                        exit[0]
-                    end
-                end
-            else
-                @continue
-            end
-        end
-    end
+            exit(0)
+         end
 
-    def stadiums
-        @stadiums = stadiums
-        Scraper.all do |name|
-            return Scraper.name
-            puts "press <enter> to continue".purple
-            response = gets.chomp
-            if response == "\n"
-                puts "type 'main' for main menu"
-                response2 = gets.chomp
-            if response2 == "main"
-                @welcome
-            else
-                @welcome
-            end
-        end
+    def stadium_name
+        @stadium = stadium_name
     end
 
     def stadium_name
         @stadium_name = stadium_name
-        puts "Enter the name of a stadium"
-        team_name = gets.chomp
-        Scraper.all.select do |name|
-            scraper.name == team_name
-            puts "hit enter to continue"
-            response = gets.chomp
-            if response == "\n"
-            @continue
-            end
-        end
-        puts "hit enter to return to the main menu"
-        respone = gets.chomp
-        if response == "\n"
-            @welcome
-        end
     end
 
-    def year_ranking
-        @year_ranking = year_ranking
-        Scraper.year_opened.sort
-        puts "hit enter to continue"
-        respone = gets.chomp
-        if response == "\n"
-            @continuing
-        end
-    end
-
-    def size_ranking
-        @size_ranking = size_ranking
-        Scraper.all.capacity.sort
-        puts "hit enter to continue"
-        respone = gets.chomp
-        if response == "\n"
-            @continuing
-        end
+    def surface
+        @surface = surface
     end
 
     def teams
         @teams = teams
-        puts "Type the name of your team. Do not include location (ie: for the Pittsburgh Steelers, enter 'Steelers' "
-        response = gets.chomp
-        Scraper.all.name.select do |name|
-        Scraper.name == response
-        @continuing
-        end
     end
 
-    def continuing
-        @continuing = continuing
-        puts "what would you like to do next?"
-        response = gets.chomp
-        @continue
+    def all
+        Scraper.all
     end
 
-    def continue
-        @continue = continue
-        if response == "list"
-            @stadiums
-        elsif response == "size"
-            @size_ranking
-        elsif response == "year"
-            @year_ranking
-        elsif response == "teams"
-            @teams
-        elsif response == "stadium"
-            @stadium_name
-        elsif response == "stop"
-            exit[0]
-        else
-            puts "invalid response. Please try again."
-            response = gets.chomp
-            timeout = 1
-                if response != "size" && response != "year" && response != "team" && response != "stadium" && response != "list"
-                    3.times do
-                    puts "Please try again."
-                    response = gets.chomp
-                    timeout +=1
-                    if timeout == 4
-                        puts "goodbye"
-                        exit[0]
-                    end
-                    @continue
-                end
-                end
-            end
-        end
-    end
-
-# end
+# binding.pry
 
--- scraper.rb
+++ scraper.rb
@@ -1,22 +1,27 @@
+#!/usr/bin/env ruby
+
 require 'nokogiri'
 require 'open_uri_redirections'
 require 'pry'
 
-# require_relative "clinteraction.rb"
-
 class Scraper
-    attr_accessor :stadium_name, :capacity, :location, :surface, :year_opened, :team_name, :stadium
+    attr_accessor :stadium_name, :capacity, :location, :surface, :year_opened, :teams, :abbr, :area, :prefix
 
 @@all = []
 @@locate = []
+@@comments = []
+@@translate = [["Alabama", "AL"], ["Alaska", "AK"], ["Arizona", "AZ"], ["Arkansas", "AR"], ["California", "CA"], ["Colorado", "CO"], ["Connecticut", "CT"], ["Delaware", "DE"], ["Florida", "FL"], ["Georgia", "GA"], ["Hawaii", "HI"], ["Idaho", "ID"], ["Illinois", "IL"], ["Indiana", "IN"], ["Iowa", "IA"], ["Kansas", "KS"], ["Kentucky", "KY"], ["Louisiana", "LA"], ["Maine", "ME"], ["Maryland", "MD"], ["Massachusetts", "MA"], ["Michigan", "MI"], ["Minnesota", "MN"], ["Mississippi", "MS"], ["Missouri", "MO"], ["Montana", "MT"], ["Nebraska", "NE"], ["Nevada",  "NV"], ["New Hampshire", "NH"], ["New Jersey", "NJ"], ["New Mexico", "NM"], ["New York", "NY"], ["North Carolina", "NC"], ["North Dakota", "ND"], ["Ohio", "OH"], ["Oklahoma", "OK"], ["Oregon", "OR"], ["Pennsylvania", "PA"], ["Rhode Island", "RI"], ["South Carolina", "SC"], ["South Dakota", "SD"], ["Tennessee", "TN"], ["Texas", "TX"], ["Utah", "UT"], ["Vermont", "VT"], ["Virginia", "VA"], ["Washington", "WA"], ["West Virginia", "WV"], ["Wisconsin", "WI"],["District of Columbia" , "DC"], ["Wyoming" , "WY"], ["Puerto Rico" , "PR"], ["Virgin Islands" , "VI"]]
 
-    def initialize(stadium_name, capacity, location, surface, teams, year_opened)
+    def initialize(stadium_name=nil, capacity=nil, location=nil, surface=nil, teams=nil, year_opened=nil)
         @stadium_name = stadium_name
         @capacity = capacity
         @surface = surface
         @location = location
         @teams = teams
         @year_opened = year_opened
+        @prefix = prefix
+        @abbr = abbr
+        @area = area
         @@all << self
     end
 
@@ -24,31 +29,66 @@ class Scraper
         @@all
     end
 
-        url = 'https://en.wikipedia.org/wiki/List_of_ZIP_Code_prefixes' #location prefix
+##prework
+        url = 'https://simple.wikipedia.org/wiki/List_of_ZIP_Code_prefixes' #zipcode/area info
         url2 = 'https://en.wikipedia.org/wiki/List_of_current_National_Football_League_stadiums' #stadium info
         url4 = 'https://www.sportingnews.com/au/nfl/list/ranking-nfl-stadiums-from-worst-to-best/1eoanvku1n1n51o76bo4o5rc9i/31' #POV ranking
 
-##prework
         location_query = URI.open(url)
-        locations = Nokogiri::HTML(location_query)
+        zip = Nokogiri::HTML(location_query)
         opinions_page = URI.open(url4)
-        scrapped_opinion = Nokogiri::HTML(opinions_page)
+        scraped_opinion = Nokogiri::HTML(opinions_page)
         stadium_query = URI.open(url2)
         stadium_info = Nokogiri::HTML(stadium_query)
-                stadium_name = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > th > a').text
-                stadium_name.gsub(/{'MetLife |Met Life''Fed Ex', 'Link', 'AT&T', 'Century|Century Link', "Energy', 'SoFi'}/, 'Fed Ex' => ' Fedex', 'Link' => 'Link', 'AT&T' => ' AT&T ', 'Century|Centurylink' => "Centurylink".chomp(""), 'Energy' => 'energy', 'MetLife\Met Life' => 'MetLife', 'SoFi' => 'So-Fi')
-                    # if str.length >
-                stadium_name = stadium_name.split(/(?<=[a-z])(?=[A-Z])/)
-                capacity = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(3)').text.chomp("\n").split("\n")
-                location = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(4) > a').text.split /(?<=[a-z])(?=[A-Z])/
-                surface = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(5)').text.chomp("\n").split("\n")
-                teams = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(7) > a').text.split /(?<=[a-z])(?=[A-Z])/
-                year_opened = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(8)').text.chomp("\n").split("\n")
-                x = 0
-                29.times do
-                    stadium = [stadium_name[x], capacity[x], location[x], surface[x], teams[x], year_opened[x]]
-                    stadium = Scraper.new(stadium_name[x], capacity[x], location[x], surface[x], teams[x], year_opened[x])
-                    x+=1
+        stadium_name = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > th > a').text
+ #Proper names; Variable inputs
+            stadium_name = stadium_name.gsub("MetLife","Metlife ")
+            stadium_name = stadium_name.gsub("FedEx"," FedEx ")
+            stadium_name = stadium_name.gsub("CenturyLink","Centurylink")
+            stadium_name = stadium_name.gsub("AT&T", "AT&T ")
+            stadium_name = stadium_name.gsub("SoFi", "So-Fi")
+            stadium_name = stadium_name.gsub("High FedEx", "HighFedex" )
+            stadium_name = stadium_name.gsub("Energy", " enegry")
+        stadium_name = stadium_name.split(/(?<=[a-z])(?=[A-Z])/)
+        capacity = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(3)').text.chomp("\n").split("\n")
+        location = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(4) > a').text.split /(?<=[a-z])(?=[A-Z])/
+        surface = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(5)').text.chomp("\n").split("\n")
+        teams = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(7) > a').text
+            teams = teams.gsub("Los Angeles RamsLos Angeles Chargers", "Los Angeles Rams, Los Angeles Chargers")
+            teams = teams.gsub("New York GiantsNew York Jets", "New York Giants, New York Jets")
+            teams = teams.split /(?<=[a-z])(?=[A-Z])/
+        year_opened = stadium_info.css('#mw-content-text > div > table:nth-child(19) > tbody > tr:nth-child(n+1) > td:nth-child(8)').text.chomp("\n").split("\n")
+    x = 0
+            while x < stadium_name.length do
+                stadium = [stadium_name[x], capacity[x], location[x], surface[x], teams[x], year_opened[x]]
+                stadium = Scraper.new(stadium_name[x], capacity[x], location[x], surface[x], teams[x], year_opened[x])
+                 x+=1
                 end
-binding.pry
+##commentary data
+
+        total = zip.css('#mw-content-text > div > table:nth-child(n+1) > tbody > tr:nth-child(n+1)').drop(6)
+
+    y = 0
+            while y < total.count do
+                total[y]
+                located = total[y].children.text.split("\n")
+                @@locate << located
+                y+=1
             end
+            # binding.pry
+    pg = 0
+        named_stadium = scraped_opinion.css('figure > div.section-label.alt-font > span.title').text.split(/(?<=[)])/)
+        comment = scraped_opinion.css('div > div > p:nth-child(2)').text
+        comment = comment.gsub("quaint.\"The Grand Old Lady\"", "quaint.The Grand Old Lady") #more poor grammer structure
+        comment = comment.split(/(?<=[[:lower:]]['.'])(?=[A-Z])|(?<=[[:digit:]]['.'])(?=[A-Z])/)
+
+        while pg < named_stadium.length do
+            commentary = [named_stadium[pg], comment[pg]]
+            pg+=1
+            @@comments << commentary
+            end
+
+        binding.pry
+
+end
+
